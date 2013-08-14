1a0009174a51611cc47d7a8398bd2a7e6efdca71
Jordan Wilson <JordanWilson@fullsail.edu>
2013-08-14 13:53:04 -0400

    Goal4 Duel3

--- Week2/goal4_duel3/js/main.js
+++ Week2/goal4_duel3/js/main.js
@@ -47,6 +47,66 @@
 
 
     function fight(){
+        //code will go here
+        //console.log("In the fight function");
+
+        //alert(fighters[0].name + ":" + fighters[0].health + " ***START*** " + fighters[1].name + ":" + fighters[1].health);
+
+        if(round < 10){
+            button.innerHTML = "FIGHT!";
+            //Random Formula - Math.floor(Math.random() * (max - min) + min);
+            var minDamage1 = fighters[0].damage * .5;
+            var minDamage2 = fighters[1].damage * .5;
+
+            var f1 = Math.floor(Math.random() * (fighters[0].damage - minDamage1) + minDamage1);
+            var f2 = Math.floor(Math.random() * (fighters[1].damage - minDamage2) + minDamage2);
+
+            //console.log(f1);
+            //console.log(f2);
+
+            //Inflict Damage
+
+
+            }
+        }
+
+    }
+
+
+    function winnerCheck(){
+        //code will go here
+        //console.log("in winnerCheck FN");
+        results = " No Winner";
+
+        if(fighters[0].health < 1 && fighters[1].health < 1){
+            document.querySelector('#round_number').innerHTML = results = "You Both Die! SHEET! ='[";
+            fighters[0].health = 100;
+            fighters[1].health = 100;
+            round = 1;
+            button.innerHTML = "Restart?";
+        }
+        else if(fighters[0].health < 1){
+            document.querySelector('#round_number').innerHTML = results = fighters[1].name + " WINS!!! VICTORY! :D";
+            fighters[0].health = 100;
+            fighters[1].health = 100;
+            round = 1;
+            button.innerHTML = "Restart?";
+        }
+        else if(fighters[1].health < 1){
+            document.querySelector('#round_number').innerHTML = results = fighters[0].name + " WINS!!! VICTORY! :D";
+            fighters[0].health = 100;
+            fighters[1].health = 100;
+            round = 1;
+            button.innerHTML = "Restart?";
+        }
+        return results;
+
+    }
+
+    //The program gets started below
+
+    //console.log("Program Starts");
+    //fight();
 
 })();
 
